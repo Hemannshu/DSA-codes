351518065a7b2ed092a897601901d921fdc02898
Himanshu Sharma <141823832+Hemannshu@users.noreply.github.com>
2024-01-25 23:39:13 +0530

    reverse a number.cpp

---
+++ reverse a number.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int revNum=0;
+    while(n>0){
+        int ld =revNum%10;
+        revNum=(revNum*10)+ld;
+        n=n/10;
+    }
+    return revNum;
+}
