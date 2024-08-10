d0e7094496f7a6476b5c02aa6e0a2403f28e2fa3
Himanshu Sharma <141823832+Hemannshu@users.noreply.github.com>
2024-08-10 13:19:12 +0530

    Create fibo.cpp

---
+++ fibo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int fib(int n){
+    if(n<=1){
+        return n;
+    }
+    int result1 = fib(n-1);
+    int result2= fib(n-2);
+    int result = result1 + result2;
+    return result;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int ans = fib(n);
+    cout<< ans<<endl;
+    return 0;
+}
