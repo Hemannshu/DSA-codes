22d097f5c9483af64a91f9f751ac6521ec059781
Himanshu Sharma <141823832+Hemannshu@users.noreply.github.com>
2024-09-17 22:22:28 +0530

    Create ChHashing.cpp

---
+++ ChHashing.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int hash[256]={0};
+    for(int i=0; i<s.size();i++){
+        hash[s[i]]++;
+    }
+int q;
+cin>>q;
+    while(q--){
+        int c;
+        cin>>c;
+        cout<<hash[c]<<endl;
+            
+    }
+    return 0;
+}
